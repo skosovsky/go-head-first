0c9949463cc704b14ded57b173365fe6a9cc130f
Sergey Kosovsky <skosovsky@gmail.com>
2024-01-10 21:15:22 +0300

    Add solution for loading

---
+++ loading/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	sizes := make(chan int)
+	urls := []string{"https://example.com/", "https://golang.org/", "https://golang.org/doc"}
+
+	for _, url := range urls {
+		go responseSize(url, sizes)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		fmt.Println(<-sizes)
+	}
+}
+
+func responseSize(url string, channel chan int) {
+	fmt.Println("Getting", url)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	channel <- len(body)
+}
