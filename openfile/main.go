6f8d24377b7dec6bfc9a3da31d032863fbf322fe
Sergey Kosovsky <skosovsky@gmail.com>
2024-01-16 20:54:54 +0300

    Add solution for openfile

--- openfile/main.go
+++ openfile/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	file, err := os.OpenFile("aardvark.txt", os.O_WRONLY, os.FileMode(0600))
+	file, err := os.OpenFile("aardvark.txt", os.O_WRONLY|os.O_APPEND|os.O_CREATE, os.FileMode(0600))
 	check(err)
 	_, err = file.Write([]byte("amazing!\n"))
 	check(err)
